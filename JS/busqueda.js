718e295a9228f8296ed7fb39f1227aa166b58d08
Juanfernandez0 <juanfernandez@udesa.edu.ar>
2023-11-16 21:55:47 -0300

    Avance buscador

---
+++ JS/busqueda.js
@@ -0,0 +1,43 @@
+
+let acaVaLaAPIKey = "9c2a46253f55c5578611eba2f0cc979c";
+
+let urlparams = new URLSearchParams(location.search);
+
+let peliculabuscada = urlparams.get('busqueda');
+
+let url = `https://api.themoviedb.org/3/search/movie?api_key=${acaVaLaAPIKey}&query=${peliculabuscada}`;
+
+let contenedorgrande = document.querySelector("#contenidobuscado")
+
+console.log(url);
+
+
+
+
+fetch(url)
+.then(function(res) {
+    return res.json();
+})
+.then(function(data) {
+    console.log(data);
+
+    let miData = data.results;
+    let contenido = " ";
+
+
+    for (let i = 0; i < miData.length; i++) {
+        
+        contenido += `<ul class="pelicula4">
+        <li><img src="https://image.tmdb.org/t/p/w500/${miData[i].poster_path}" class="portada"></li>
+        <li><a href="./pelicula.html?id=${miData[i].id}" class="titulopelicula" target="_blank" style="color: aliceblue;">${miData[i].title}</a></li>
+        <li><a href="./pelicula.html?id=${miData[i].id}" class="titulopelicula" target="_blank" style="color: aliceblue;">Fecha de estreno : ${miData[i].release_date}</a></li>
+      </ul>`
+        
+    }
+
+    contenedorgrande.innerHTML = contenido
+})
+
+.catch(function(error) {
+    console.log(error);
+})
